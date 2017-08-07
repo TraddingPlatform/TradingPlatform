d80e3492ba262e6f779e294827fafd2e0e0bac6d
VictorWuX <victorwux@163.com>
2017-08-06 22:45:01 -0700

    server is ok

---
+++ Code/TradingPlatform/src/main/java/org/citi/training/TradingPlatform/server/StartServer.java
@@ -0,0 +1,29 @@
+package org.citi.training.TradingPlatform.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+
+public class StartServer {
+	public static void main( String[] args ) throws Exception
+    {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("bean.xml");
+		
+		Server server=new Server(8080);
+    	ServletHolder servlet = new ServletHolder(ServletContainer.class);
+    	servlet.setInitParameter("com.sun.jersey.config.property.resourceConfigClass", 
+    			"com.sun.jersey.api.core.PackagesResourceConfig");
+        servlet.setInitParameter("com.sun.jersey.config.property.packages", 
+        		"org.citi.training.TradingPlatform.facade");
+        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        handler.setContextPath("/");
+        handler.addServlet(servlet, "/*");
+        server.setHandler(handler);
+        server.start();
+        server.join();
+    }
+}
